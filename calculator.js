16afbb5f91a4146baa660abf70343b88722aa800
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-08 19:28:03 -1000

    day 2 hwk -1

--- calculator.js
+++ calculator.js
@@ -7,23 +7,47 @@
  */
 
 
-  /**
+var calculatorModule = function() {
+
+// private variables
+  var _total = 0;
+  var _memory = 0;
+
+
+/**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+  function setTotal(x){
+     if (typeof x !== "number"){
+        throw Error("not a number");
+   }
+   _total = x;
+   return _total;
+  }
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+  function getTotal(){
+    return _total;
+  }
 
-  /**
+/**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
+
+   function add(x){
+     if (typeof x !== "number"){
+        throw new Error("not a number");
+   }
+    _total = _total + x;
+    return _total;
+   }
 
 
   /**
@@ -31,35 +55,91 @@
    * @param  { Number } x
    */
 
+   function subtract(x){
+     if (typeof x !== "number"){
+        throw new Error("not a number");
+   }
+    _total = _total - x;
+    return _total;
+   }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+   function multiply(x){
+     if (typeof x !== "number"){
+        throw new Error("not a number");
+   }
+    _total = _total * x;
+    return _total;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   function divide(x){
+     if (typeof x !== "number"){
+        throw new Error("not a number");
+    }
+    _total = _total/x;
+
+    return _total;
+   }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+   function recallMemory(){
+    return _memory;
+   }
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+   function saveMemory(){
+    _memory = _total;
+    return  _memory;
+   }
 
   /**
    * Clear the value stored at `memory`
    */
+   function clearMemory(){
+    _memory = 0;
+    return _memory;
+   }
 
   /**
    * Validation
    */
+
+
+
+
+return {
+    load: setTotal,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory:recallMemory,
+    saveMemory:saveMemory,
+    clearMemory: clearMemory,
+
+
+   };
+
+
+
+// var calculator = {};
+
+// return calculator;
+
+ };
+
 
